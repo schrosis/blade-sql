3126a019c9e5c36ef5ec0adc0a69d4d8f143208e
schrosis <54675948+schrosis@users.noreply.github.com>
2021-03-13 22:20:16 +0900

    [add]Methods to convert from stdClass to entities and models

--- src/BladeSQL/SelectResultCollection.php
+++ src/BladeSQL/SelectResultCollection.php
@@ -2,8 +2,71 @@
 
 namespace Schrosis\BladeSQL\BladeSQL;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
+use InvalidArgumentException;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionMethod;
 
 class SelectResultCollection extends Collection
 {
+    public function entity(string $entityClass): Collection
+    {
+        if (! $this->hasFromArrayMethod($entityClass)) {
+            throw new InvalidArgumentException(
+                'The argument entityClass must have a fromArray method'
+            );
+        }
+
+        return $this
+            ->toBase()
+            ->map(function ($item) use ($entityClass) {
+                return [$entityClass, 'fromArray']((array)$item);
+            });
+    }
+
+    private function hasFromArrayMethod(string $class): bool
+    {
+        try {
+            $refMethod = new ReflectionMethod($class, 'fromArray');
+            return $refMethod->isPublic() && $refMethod->isStatic();
+        } catch (ReflectionException $e) {}
+
+        return false;
+    }
+
+    /**
+     * map to model
+     *
+     * @param string|Model $modelClass
+     * @return Collection
+     */
+    public function model($model): Collection
+    {
+        if (is_string($model)) {
+            if (! $this->isModelClass($model)) {
+                throw new InvalidArgumentException(
+                    'If the argument model is a string, it must have a fromArray method'
+                );
+            }
+            $model = new $model;
+        }
+
+        return $this
+            ->toBase()
+            ->map(function($item) use ($model) {
+                return $model->newFromBuilder($item);
+            });
+    }
+
+    private function isModelClass(string $modelClass): bool
+    {
+        try {
+            $refClass = new ReflectionClass($modelClass);
+            return $refClass->isSubclassOf(Model::class);
+        } catch (ReflectionException $e) {}
+
+        return false;
+    }
 }
---
+++ tests/Unit/SelectResultCollectionTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Schrosis\BladeSQL\Tests\Unit;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+use InvalidArgumentException;
+use Schrosis\BladeSQL\BladeSQL\SelectResultCollection;
+use Schrosis\BladeSQL\Tests\TestCase;
+
+class ExampleEntity
+{
+    public static function fromArray(array $data)
+    {
+        return new self;
+    }
+}
+
+class NotEntity {}
+
+class ExampleModel extends Model {}
+
+class NotModel {}
+
+class SelectResultCollectionTest extends TestCase
+{
+    public function testParentClass()
+    {
+        $selectResult = new SelectResultCollection();
+        $this->assertInstanceOf(Collection::class, $selectResult);
+    }
+
+    public function testEntity()
+    {
+        $selectResult = new SelectResultCollection([[
+            'id' => 1,
+            'name' => 'sample data',
+        ]]);
+        $entities = $selectResult->entity(ExampleEntity::class);
+
+        $this->assertInstanceOf(Collection::class, $entities);
+        $this->assertNotInstanceOf(SelectResultCollection::class, $entities);
+
+        foreach ($entities->all() as $entity) {
+            $this->assertInstanceOf(ExampleEntity::class, $entity);
+        }
+    }
+
+    public function testThrowExceptionWhenNotEntityClass()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The argument entityClass must have a fromArray method');
+
+        $selectResult = new SelectResultCollection([[
+            'id' => 1,
+            'name' => 'sample data',
+        ]]);
+        $selectResult->entity(NotEntity::class);
+    }
+
+    public function testModel()
+    {
+        $selectResult = new SelectResultCollection([[
+            'id' => 1,
+            'name' => 'sample data',
+        ]]);
+        $models = $selectResult->model(ExampleModel::class);
+
+        $this->assertInstanceOf(Collection::class, $models);
+        $this->assertNotInstanceOf(SelectResultCollection::class, $models);
+
+        foreach ($models->all() as $model) {
+            $this->assertInstanceOf(ExampleModel::class, $model);
+        }
+    }
+
+    public function testThrowExceptionWhenNotModelClass()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('If the argument model is a string, it must have a fromArray method');
+
+        $selectResult = new SelectResultCollection([[
+            'id' => 1,
+            'name' => 'sample data',
+        ]]);
+        $selectResult->model(NotModel::class);
+    }
+
+    public function testThrowExceptionWhenNotClass()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('If the argument model is a string, it must have a fromArray method');
+
+        $selectResult = new SelectResultCollection([[
+            'id' => 1,
+            'name' => 'sample data',
+        ]]);
+        $selectResult->model('something string');
+    }
+}
