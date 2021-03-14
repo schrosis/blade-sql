e2694715288db8859f2f0c06606dcc5dba7ded46
schrosis <54675948+schrosis@users.noreply.github.com>
2021-03-15 00:50:16 +0900

    [add]comment config file

--- src/config/blade-sql.php
+++ src/config/blade-sql.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
+    /**
+     * Blade directives and component prefixes provided by blade-sql
+     */
     'prefix' => '',
+    /**
+     * Directory to put SQL blade files
+     */
     'dir' => resource_path('sql'),
 ];
